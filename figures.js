7c1870b7e02cf22fb83df2c00f5becc49fa59428
Thomas Tursics <thomas@tursics.de>
2015-10-14 07:54:15 +0200

    initial version

---
+++ figures.js
@@ -0,0 +1,354 @@
+//
+// this file includes all figures of 'Jugend Hackt'
+//
+// please use following block encoding:
+// b  blue
+// g  green
+// k  black
+// r  red
+// w  white
+// y  yellow
+// t  semi-transparent
+// |  end of line
+// use a space for a non-block
+//
+
+figures = [
+' bb     bb |'+
+'bbbb   bbbb|'+
+'bbbbbbbbbbb|'+
+'bbbbbbbbbbb|'+
+'bbwwwwwwwbb|'+
+'bwwwwwwwwwb|'+
+'bwkwwwwwkwb|'+
+'bwwwkwkwwwb|'+
+'bbwwkkkwwbb|'+
+' bbwwwwwbb |'+
+'  bbbbbbb  '
+,
+' yy     yy |'+
+'yyyy   yyyy|'+
+'yyyyyyyyyyy|'+
+'yyyyyyyyyyy|'+
+'yywwwwwwwyy|'+
+'ywwwwwwwwwy|'+
+'ywkwwwwwkwy|'+
+'ywwwkwkwwwy|'+
+'yywwkkkwwyy|'+
+' yywwwwwyy |'+
+'  yyyyyyy  '
+,
+'rrrrrrrrrrrr   rrrrrrrrrrrr|'+
+'rrrrrrrrrrrr   rrrrrrrrrrrr|'+
+'rrtttttttttrrrrrtttttttttrr|'+
+' rttbbwwtttrrrrrttbbwwtttr |'+
+' rttbbwwtttrr rrttbbwwtttr |'+
+' rtttttttttr   rtttttttttr |'+
+' rrtttttttrr   rrtttttttrr |'+
+'  rrtttttrr     rrtttttrr  |'+
+'   rrrrrrr       rrrrrrr   '
+,
+'kkkkkkkkkkkk   kkkkkkkkkkkk|'+
+'kkkkkkkkkkkk   kkkkkkkkkkkk|'+
+'kktttttttttkkkkktttttttttkk|'+
+' ktttwwbbttkkkkktttwwbbttk |'+
+' ktttwwbbttkk kktttwwbbttk |'+
+' ktttttttttk   ktttttttttk |'+
+' kktttttttkk   kktttttttkk |'+
+'  kktttttkk     kktttttkk  |'+
+'   kkkkkkk       kkkkkkk   '
+,
+'      kkkkkkkkkkkkkkk         |'+
+'     kbbbbbbbbbbbbbbbk        |'+
+'     kbbbbbbbbbbbbbbbk        |'+
+'     kbbbbbbbbbbbbbbbk        |'+
+'     kbbbbbbbbbbbbbbbk        |'+
+'     kbbbbbbbbbbbbbbbk        |'+
+'     kbbbbbbbbbbbbbbbk        |'+
+'     kbbbbbbbbbbbbbbbk        |'+
+'     kbbbbbbbbbbbbbbbk        |'+
+'     kbbbbbbbbbbbbbbbk        |'+
+'     kkkkkkkkkkkkkkkkk        |'+
+'    kyyyyyyyyyyyyyyyyyk       |'+
+'    kykykykykykykykykyk       |'+
+'   kyyyyyyyyyyyyyyyyyyyk      |'+
+'   kykykykkkkkkkkkykykyk      |'+
+'  kwkwkwkyyyyyyyyyyyyyyk      |'+
+'  kwkwkwkkkkkkkkkkkkkkk       |'+
+' kkwkwkwk                     |'+
+'kwkwkwkwk        k            |'+
+'kwkwkwkwk       kwk           |'+
+'kwkwkwkwk       kwwk          |'+
+'kwwwwwwwk        kwkkkkkk     |'+
+'kwwwwwwwk k      kwkwkwkwk    |'+
+'kwwwwwwwkkwk     kwkwkwkwk    |'+
+'kwwwwwwwkwwk   k kwkwkwkwk    |'+
+' kwwwwwwwwk   kwkkwwwwwwwk    |'+
+' kwwwwwwwwk   kwwkwwwwwwwwk   |'+
+' kwwwwwwwk     kwwwwwwwwwwk   |'+
+'  kwwwwwwk     kwwwwwwwwwwk   |'+
+'  kwwwwwk       kwwwwwwwwwk   |'+
+'  kwwwwwk       kwwwwwwwwwk   |'+
+'  kwwwwwk        kwwwwwwwwwk  |'+
+'  kkkkkkk         kkwwwwwwwwk |'+
+'                    kwwwwwwwwk|'+
+'                     kwwwwwwk |'+
+'                     kwwwwwk  |'+
+'                      kwwwk   |'+
+'                       kkk    '
+,
+'         kkkkkkkkkkkkkkk      |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kkkkkkkkkkkkkkkkk     |'+
+'       kyyyyyyyyyyyyyyyyyk    |'+
+'       kykykykykykykykykyk    |'+
+'      kyyyyyyyyyyyyyyyyyyyk   |'+
+'      kykykykkkkkkkkkykykyk   |'+
+'      kyyyyyyyyyyyyyykwkwkwk  |'+
+'       kkkkkkkkkkkkkkkwkwkwk  |'+
+'                     kwkwkwkk |'+
+'            k        kwkwkwkwk|'+
+'           kwk       kwkwkwkwk|'+
+'          kwwk       kwkwkwkwk|'+
+'     kkkkkkwk        kwwwwwwwk|'+
+'    kwkwkwkwk      k kwwwwwwwk|'+
+'    kwkwkwkwk     kwkkwwwwwwwk|'+
+'    kwkwkwkwk k   kwwkwwwwwwwk|'+
+'    kwwwwwwwkkwk   kwwwwwwwwk |'+
+'   kwwwwwwwwkwwk   kwwwwwwwwk |'+
+'   kwwwwwwwwwwk     kwwwwwwwk |'+
+'   kwwwwwwwwwwk     kwwwwwwk  |'+
+'   kwwwwwwwwwk       kwwwwwk  |'+
+'   kwwwwwwwwwk       kwwwwwk  |'+
+'  kwwwwwwwwwk        kwwwwwk  |'+
+' kwwwwwwwwkk         kkkkkkk  |'+
+'kwwwwwwwwk                    |'+
+' kwwwwwwk                     |'+
+'  kwwwwwk                     |'+
+'   kwwwk                      |'+
+'    kkk                       '
+,
+'         kkkkkkkkkkkkkkk      |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kbbbbbbbbbbbbbbbk     |'+
+'        kkkkkkkkkkkkkkkkk     |'+
+'       kgggggggggggggggggk    |'+
+'       kgkgkgkgkgkgkgkgkgk    |'+
+'      kgggggggggggggggggggk   |'+
+'      kgkgkgkkkkkkkkkgkgkgk   |'+
+'      kggggggggggggggkwkwkwk  |'+
+'       kkkkkkkkkkkkkkkwkwkwk  |'+
+'                     kwkwkwkk |'+
+'            k        kwkwkwkwk|'+
+'           kwk       kwkwkwkwk|'+
+'          kwwk       kwkwkwkwk|'+
+'     kkkkkkwk        kwwwwwwwk|'+
+'    kwkwkwkwk      k kwwwwwwwk|'+
+'    kwkwkwkwk     kwkkwwwwwwwk|'+
+'    kwkwkwkwk k   kwwkwwwwwwwk|'+
+'    kwwwwwwwkkwk   kwwwwwwwwk |'+
+'   kwwwwwwwwkwwk   kwwwwwwwwk |'+
+'   kwwwwwwwwwwk     kwwwwwwwk |'+
+'   kwwwwwwwwwwk     kwwwwwwk  |'+
+'   kwwwwwwwwwk       kwwwwwk  |'+
+'   kwwwwwwwwwk       kwwwwwk  |'+
+'  kwwwwwwwwwk        kwwwwwk  |'+
+' kwwwwwwwwkk         kkkkkkk  |'+
+'kwwwwwwwwk                    |'+
+' kwwwwwwk                     |'+
+'  kwwwwwk                     |'+
+'   kwwwk                      |'+
+'    kkk                       '
+,
+'       kk                       |'+
+'      kkkk            gggg      |'+
+'     kkkkkk          kggggg     |'+
+'     kwwwwk          kwwwwk     |'+
+'      wwww           kwwwwk     |'+
+'      wwww            wwww   kkk|'+
+'       ww              ww    kkk|'+
+'    bbbbbbbb        rrrrrrrr kwk|'+
+'   bbbbbbbbbb      rrrrrrrrrr r |'+
+'  bb bbbbbb bb    rr rrrrrr rrr |'+
+' bb  bbbbbb  bb  rr  rrrrrr  r  |'+
+' b   bbbbbb   bbrr   rrrrrr     |'+
+'kwk  bbbbbb    ww    rrrrrr     |'+
+'kkk  bbbbbb          rrrrrr     |'+
+'kkk  bbbbbb          yyyyyy     |'+
+'     bbbbbb          yyyyyy     |'+
+'     bbbbbb          yy  yy     |'+
+'  rrggg  gg        yyyy  yy     |'+
+'  rrgg   gg       byyy   yy     |'+
+'  r     ggg       b     yyy     |'+
+'      rrgg             yyy      |'+
+'      rrg             byy       |'+
+'      r               b         '
+,
+'       kk                       |'+
+'      kkkk            yyyy      |'+
+'     kkkkkk          kyyyyy     |'+
+'     kwwwwk          kwwwwk     |'+
+'      wwww           kwwwwk     |'+
+'      wwww            wwww   kkk|'+
+'       ww              ww    kkk|'+
+'    rrrrrrrr        gggggggg kwk|'+
+'   rrrrrrrrrr      gggggggggg g |'+
+'  rr rrrrrr rr    gg gggggg ggg |'+
+' rr  rrrrrr  rr  gg  gggggg  g  |'+
+' r   rrrrrr   rrgg   gggggg     |'+
+'kwk  rrrrrr    ww    gggggg     |'+
+'kkk  rrrrrr          gggggg     |'+
+'kkk  rrrrrr          yyyyyy     |'+
+'     rrrrrr          yyyyyy     |'+
+'     rrrrrr          yy  yy     |'+
+'  yybbb  bb        yyyy  yy     |'+
+'  yybb   bb       byyy   yy     |'+
+'  y     bbb       b     yyy     |'+
+'      yybb             yyy      |'+
+'      yyb             byy       |'+
+'      y               b         '
+,
+'            kkk |'+
+'           kbbbk|'+
+' kkkkkkkkkkkbbbk|'+
+'kyyyyyyyyyykbbbk|'+
+' kkkkkkkkkkkbbbk|'+
+'           kbbbk|'+
+'            kbk |'+
+'             k  '
+,
+' kkk            |'+
+'kbbbk           |'+
+'kbbbkkkkkkkkkkk |'+
+'kbbbkyyyyyyyyyyk|'+
+'kbbbkkkkkkkkkkk |'+
+'kbbbk           |'+
+' kbk            |'+
+'  k             '
+,
+'       k       |'+
+'      kwk      |'+
+'     kwwwk     |'+
+'    kwwkwwk    |'+
+'   kwwk kwwk   |'+
+'  kwwk   kwwk  |'+
+' kwwkkkkkkkwwk |'+
+'kwwwwwwwwwwwwwk|'+
+' kkkkkkkkkkkkk '
+,
+'    bbbbbbbb    |'+
+'   bbbbbbbbbb   |'+
+'  bbbbbbbbbbbb  |'+
+' bbbbbbbbbbbbbb |'+
+'bbbbbbbbbbbbbbbb|'+
+'bbbbbbbbbbbbbbbb|'+
+'bbbbbbbbbbbbbbbb|'+
+'bggggggbbggggggb|'+
+'bggggggggggggggb|'+
+'bgkkkkggggkkkkgb|'+
+'bggkkkggggkkkggb|'+
+'bbggggggggggggbb|'+
+'bbbggggggggggbbb|'+
+'bbbbbbgbbgbbbbbb|'+
+'bbbbbbggggbbbbbb|'+
+'bbbbggggggggbbbb|'+
+'bbbbggkkkkggbbbb|'+
+'bbbbggkkkkggbbbb|'+
+'bbbbggggggggbbbb|'+
+' bbbbbbbbbbbbbb |'+
+'  bbbbbbbbbbbb  |'+
+'    bbbbbbbb    |'+
+'    bbbbbbbb    |'+
+'     bbbbbb     '
+,
+'      rrr      |'+
+'     r   r     |'+
+'  rrrrrrrrrrr  |'+
+' rrrrrrrrrrrrr |'+
+' rrrrrrrrrrrrr |'+
+' rrrrrrrrrrrrr |'+
+' rrrrrrrrrrrrr |'+
+' rrrrrrrrrrrrr |'+
+' rrkkrrrrrkkrr |'+
+'rrkrrkrrrkrrkrr|'+
+'rrkrrkrrrkrrkrr|'+
+'rrrkkrrrrrkkrrr|'+
+' rrrrrrrrrrrrr |'+
+' rrkkkkkkkkkrr |'+
+' rkrkrkrkrkrkr |'+
+' rkrkrkrkrkrkr |'+
+' rrkkkkkkkkkrr |'+
+'  rrrrrrrrrrr  |'+
+'               |'+
+' r rrrrrrrrr r |'+
+' r rrrrrrrrr r |'+
+' r rrrrrrrrr r |'+
+' r rrrrrrrrr r |'+
+' r rrrrwrrrr r |'+
+' r rrrwrwrrr r |'+
+' r rrwrrrwrr r '
+,
+'  bbbbbbbbb  |'+
+' bbbbbbbbbbb |'+
+'bbbbbbbbbbbbb|'+
+'bbbbbbbbbbbbb|'+
+'bbbbbbbbbbbbb|'+
+'bbbbbbbbbbbbb|'+
+'bbbbbbbbbbbbb|'+
+'bbbbbbbbbbbbb|'+
+'bbkkbbbbbkkbb|'+
+'bbkkkbbbkkkbb|'+
+'bbbbbbbbbbbbb|'+
+' bbbbkbkbbbb |'+
+'  bbbbbbbbb  |'+
+'   bbkkkbb   |'+
+'    bbbbb    |'+
+'     bbb     |'+
+'    bbbbb    |'+
+'   bbbbbbb   |'+
+'  bbbbbbbbb  |'+
+'  bbbbbbbbb  |'+
+'  bbbbbbbbb  |'+
+'  b bbbbb b  |'+
+'  b bbbbb b  '
+,
+'      gggwwggg      |'+
+'     gggwggwggg     |'+
+'    gggwggggwggg    |'+
+'   gggwggggggwggg   |'+
+'  gggwggggggggwggg  |'+
+' gggggggggggggggggg |'+
+'gggggggggggggggggggg|'+
+'gggggkkkggggkkkggggg|'+
+'gggggggggggggggggggg|'+
+'gggggkkkggggkkkggggg|'+
+'gggggkkkggggkkkggggg|'+
+' gggggggggggggggggg |'+
+'   gggggggggggggg   |'+
+'     ggggwwgggg     |'+
+'      gggggggg      |'+
+'      gggggggg      |'+
+'      g gggg g      |'+
+'       g gg g       |'+
+'      g g  g g      |'+
+' g     g gg g     g |'+
+'  g   g g  g g   g  |'+
+'   g g g gg g g g   |'+
+'    g g g  g g g    |'+
+'     g g gg g g     |'
+];
